dbbcdd1479b5f54d77fec5610e5e597502a253e1
devCJH <http-@naver.com>
2022-10-14 20:01:31 +0900

    MVC 구조 도입

--- src/com/KoreaIT/example/JAM/controller/ArticleController.java
+++ src/com/KoreaIT/example/JAM/controller/ArticleController.java
@@ -1,21 +1,16 @@
 package com.KoreaIT.example.JAM.controller;
 
 import java.sql.Connection;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
 
 import com.KoreaIT.example.JAM.Article;
 import com.KoreaIT.example.JAM.service.ArticleService;
-import com.KoreaIT.example.JAM.util.DBUtil;
-import com.KoreaIT.example.JAM.util.SecSql;
 
 public class ArticleController extends Controller {
-	
+
 	private ArticleService articleService;
-	private Connection conn;
-	
+
 	public ArticleController(Connection conn, Scanner sc) {
 		super(sc);
 		articleService = new ArticleService(conn);
@@ -35,26 +30,15 @@ public class ArticleController extends Controller {
 	}
 
 	public void showList(String cmd) {
-		System.out.println("== 게시물 리스트 ==");
 
-		List<Article> articles = new ArrayList<>();
-
-		SecSql sql = new SecSql();
-
-		sql.append("SELECT *");
-		sql.append("FROM article");
-		sql.append("ORDER BY id DESC;");
-
-		List<Map<String, Object>> articleListMap = DBUtil.selectRows(conn, sql);
-
-		for (Map<String, Object> articleMap : articleListMap) {
-			articles.add(new Article(articleMap));
-		}
+		List<Article> articles = articleService.getArticles();
 
 		if (articles.size() == 0) {
 			System.out.println("게시물이 없습니다");
 			return;
 		}
+
+		System.out.println("== 게시물 리스트 ==");
 
 		System.out.println("번호	|	제목");
 
@@ -62,27 +46,18 @@ public class ArticleController extends Controller {
 			System.out.printf("%d	|	%s\n", article.id, article.title);
 		}
 	}
-	
+
 	public void showDetail(String cmd) {
 		int id = Integer.parseInt(cmd.split(" ")[2]);
 
-		SecSql sql = new SecSql();
+		Article article = articleService.getArticle(id);
 
-		sql.append("SELECT *");
-		sql.append("FROM article");
-		sql.append("WHERE id = ?", id);
-
-		Map<String, Object> articleMap = DBUtil.selectRow(conn, sql);
-
-		if (articleMap.isEmpty()) {
+		if (article == null) {
 			System.out.printf("%d번 게시글은 존재하지 않습니다\n", id);
 			return;
 		}
 
 		System.out.printf("== %d번 게시물 상세보기 ==\n", id);
-
-		Article article = new Article(articleMap);
-
 		System.out.printf("번호 : %d\n", article.id);
 		System.out.printf("작성날짜 : %s\n", article.regDate);
 		System.out.printf("수정날짜 : %s\n", article.updateDate);
@@ -92,6 +67,14 @@ public class ArticleController extends Controller {
 
 	public void doModify(String cmd) {
 		int id = Integer.parseInt(cmd.split(" ")[2]);
+
+		boolean isArticleExists = articleService.isArticleExists(id);
+
+		if (isArticleExists == false) {
+			System.out.printf("%d번 게시글은 존재하지 않습니다\n", id);
+			return;
+		}
+
 		System.out.printf("== %d번 게시물 수정 ==\n", id);
 
 		System.out.printf("수정할 제목 : ");
--- src/com/KoreaIT/example/JAM/service/ArticleService.java
+++ src/com/KoreaIT/example/JAM/service/ArticleService.java
@@ -1,7 +1,9 @@
 package com.KoreaIT.example.JAM.service;
 
 import java.sql.Connection;
+import java.util.List;
 
+import com.KoreaIT.example.JAM.Article;
 import com.KoreaIT.example.JAM.dao.ArticleDao;
 
 public class ArticleService {
@@ -26,6 +28,14 @@ public class ArticleService {
 	
 	public boolean isArticleExists(int id) {
 		return articleDao.isArticeExists(id);
+	}
+
+	public Article getArticle(int id) {
+		return articleDao.getArticle(id);
+	}
+
+	public List<Article> getArticles() {
+		return articleDao.getArticles();
 	}
 
 }
